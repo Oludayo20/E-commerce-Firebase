5967a359c9c769479cb884219273a52c866f50d4
Oludayo <akinpelu.dayo11@gmail.com>
2023-06-11 22:38:09 +0100

    Week End

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import products from '../assets/data/products';
 import Helmet from '../components/Helmet/Helmet';
 import CommonSection from '../components/UI/CommonSection';
 import { Container, Col, Row } from 'reactstrap';
 import { useParams } from 'react-router-dom';
 import '../styles/productDetails.css';
+import { motion } from 'framer-motion';
 
 const ProductDetails = () => {
+  const [tab, setTab] = useState('des');
+
   const { id } = useParams();
   const product = products.find((item) => item.id === id);
 
@@ -15,13 +18,13 @@ const ProductDetails = () => {
     productName,
     price,
     avgRating,
-    review,
+    reviews,
     description,
     shortDesc
   } = product;
 
   return (
-    <Helmet>
+    <Helmet title={productName}>
       <CommonSection />
       <section className="pt-0">
         <Container>
@@ -32,25 +35,25 @@ const ProductDetails = () => {
             <Col lg="6">
               <div className="product_details">
                 <h2>{productName}</h2>
-                <div className="product_rating">
-                  <div className="">
+                <div className="product_rating d-flex align-items-center gap-5 mb-3">
+                  <div>
                     <span>
-                      <i className="ri-start-s-fill"></i>
+                      <i className="ri-star-s-fill"></i>
                     </span>
                     <span>
-                      <i className="ri-start-s-fill"></i>
+                      <i className="ri-star-s-fill"></i>
                     </span>
                     <span>
-                      <i className="ri-start-s-fill"></i>
+                      <i className="ri-star-s-fill"></i>
                     </span>
                     <span>
-                      <i className="ri-start-s-fill"></i>
+                      <i className="ri-star-s-fill"></i>
                     </span>
                     <span>
-                      <i className="ri-start-s-fill"></i>
+                      <i className="ri-star-s-fill"></i>
                     </span>
                     <span>
-                      <i className="ri-start-s-fill"></i>
+                      <i className="ri-star-s-fill"></i>
                     </span>
                   </div>
                   <p>
@@ -58,12 +61,47 @@ const ProductDetails = () => {
                   </p>
                 </div>
 
-                <span>${price}</span>
+                <span className="product_price">${price}</span>
 
-                <p>{shortDesc}</p>
+                <p className="mt-3">{shortDesc}</p>
 
-                <button className="buy_btn">Add to Cart</button>
+                <motion.button whileTap={{ scale: 1.1 }} className="buy_btn">
+                  Add to Cart
+                </motion.button>
               </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12">
+              <div className="tab_wrapper d-flex align-items-center gap-5">
+                <h6
+                  className={`${tab === 'desc' ? 'active_tab' : ''}`}
+                  onClick={() => setTab('desc')}
+                >
+                  Description
+                </h6>
+                <h6
+                  className={`${tab === 'rev' ? 'active_tab' : ''}`}
+                  onClick={() => setTab('rev')}
+                >
+                  Reviews({reviews.length})
+                </h6>
+              </div>
+
+              {tab == 'desc' ? (
+                <div className="tab_content mt-5">
+                  <p>{description}</p>
+                </div>
+              ) : (
+                <div className="tab_content mt-5">
+                  <p>reviews</p>
+                </div>
+              )}
             </Col>
           </Row>
         </Container>
